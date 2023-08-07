4129c882d7f7378711f7a987edfbf856fe46c363
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-08-07 21:13:32 +0300

    added NavLayout.jsx

---
+++ src/components/NavLayout.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Link, Outlet } from 'react-router-dom'
+export default function NavLayout() {
+    return (
+        <div className="nav--container">
+            <header>
+                <Link to="/" className="logo">lotrip</Link>
+                <nav>
+                    <Link to="signup">Sign up</Link>
+                    <Link to="login">Login</Link>
+                </nav>
+            </header>
+            <Outlet/>
+        </div>
+    )
+}
